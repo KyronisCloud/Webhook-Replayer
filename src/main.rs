3dc3fe317322bd4831d18e954a0f027c779874a1
Mike Nguyen <ducthien562000@gmail.com>
2026-06-13 21:59:01 +0700

    add logger

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,24 @@
 use axum::{Router, routing::get};
+use tracing::{Level, info};
 
 #[tokio::main]
 async fn main() {
+    // Initialize the subscriber with JSON formatting
+    tracing_subscriber::fmt()
+        .json()
+        .with_max_level(Level::INFO)
+        .with_target(true) // Include the module path
+        .with_current_span(true) // Include the current span
+        .with_span_list(true) // Include the full span hierarchy
+        .init();
+
     let app = Router::new().route("/", get(hello_world));
 
     let listener = tokio::net::TcpListener::bind("127.0.0.1:3210")
         .await
         .unwrap();
 
-    println!("Listening on: http://{}", listener.local_addr().unwrap());
-
+    info!("Listening on: http://{}", listener.local_addr().unwrap());
     axum::serve(listener, app).await.unwrap();
 }
 
