4f9c35e0a8dde9f774dbe43b7b6165f1da7997a4
Mike Nguyen <ducthien562000@gmail.com>
2026-06-13 21:43:16 +0700

    init web server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{Router, routing::get};
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(hello_world));
+
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3210")
+        .await
+        .unwrap();
+
+    println!("Listening on: http://{}", listener.local_addr().unwrap());
+
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn hello_world() -> &'static str {
+    "Hello, World!"
 }
